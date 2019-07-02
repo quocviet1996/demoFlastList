2cd81cd04c05a856c7e0b72fa2d62b79779cd9f5
quocviet1996 <quocquocviet1996@gmail.com>
2019-07-02 21:53:50 +0700

    first commit

---
+++ src/component/flastlistData.js
@@ -0,0 +1,57 @@
+var mang =[
+    {
+        key:1,
+       image:"https://znews-photo.zadn.vn/w210/Uploaded/ofh_hmzpgazs/2019_05_03/59252976_1044869805713163_3508535256271552512_n.jpg",
+       title:" Hàng trăm người mang logo 'uống rượu không lái xe' tiễn đưa cô giáo",
+       noidung:"Hàng trăm người mang logo 'uống rượu không lái xe' tiễn đưa cô giáo",
+   },
+   {
+       key:2,
+       image:"https://znews-photo.zadn.vn/w210/Uploaded/ofh_hmzpgazs/2019_05_03/59252976_1044869805713163_3508535256271552512_n.jpg",
+       title:" Thủ tướng yêu cầu báo cáo tính đúng, sai của việc tăng giá điện",
+       noidung:"Hàng trăm người mang logo 'uống rượu không lái xe tiễn đưa cô giáo",
+   },
+   {
+       key:3,
+       image:"https://znews-photo.zadn.vn/w210/Uploaded/ofh_hmzpgazs/2019_05_03/59252976_1044869805713163_3508535256271552512_n.jpg",
+       title:" Đội bóng Tây Ban Nha định chiêu mộ Quang Hải mạnh cỡ nào?",
+       noidung:"Hàng trăm người mang logo 'uống rượu không lái xe' tiễn đưa cô giáo"
+   },
+   {
+       key:4,
+       image:"https://znews-photo.zadn.vn/w210/Uploaded/ofh_hmzpgazs/2019_05_03/59252976_1044869805713163_3508535256271552512_n.jpg",
+       title:" Hàng trăm người mang logo 'uống rượu không lái xe' tiễn đưa cô giáo",
+       noidung:"Hàng trăm người mang logo 'uống rượu không lái xe' tiễn đưa cô giáo",
+   },
+   {
+       key:5,
+       image:"https://znews-photo.zadn.vn/w210/Uploaded/ofh_hmzpgazs/2019_05_03/59252976_1044869805713163_3508535256271552512_n.jpg",
+       title:"   Vụ thảm sát ở Bình Tân: Kẻ gây án hoang tưởng nạn nhân là robot",
+       noidung:"Hàng trăm người mang logo 'uống rượu không lái xe' tiễn đưa cô giáo",
+   },
+   {
+       key:6,
+       image:"https://znews-photo.zadn.vn/w210/Uploaded/ofh_hmzpgazs/2019_05_03/59252976_1044869805713163_3508535256271552512_n.jpg",
+       title:" Hot girl Trâm Anh tiếp tục bị cắt sóng khỏi game show trên VTV  ",
+       noidung:"Hàng trăm người mang logo 'uống rượu không lái xe' tiễn đưa cô giáo",
+   },
+   {
+       key:7,
+       image:"https://znews-photo.zadn.vn/w210/Uploaded/ofh_hmzpgazs/2019_05_03/59252976_1044869805713163_3508535256271552512_n.jpg",
+       title:" Nữ sinh 20 tuổi gây xúc động khi đỡ 8 nhát dao để cứu bạn",
+       noidung:"Hàng trăm người mang logo 'uống rượu không lái xe' tiễn đưa cô giáo",
+   },
+   {
+       key:8,
+       image:"https://znews-photo.zadn.vn/w210/Uploaded/ofh_hmzpgazs/2019_05_03/59252976_1044869805713163_3508535256271552512_n.jpg",
+       title:" Thị trường điện thoại bão hòa, Thế giới Di động đóng cửa 48 cửa hàng",
+       noidung:"Hàng trăm người mang logo 'uống rượu không lái xe' tiễn đưa cô giáo",
+   },
+   {
+       key:9,
+       image:"https://znews-photo.zadn.vn/w210/Uploaded/ofh_hmzpgazs/2019_05_03/59252976_1044869805713163_3508535256271552512_n.jpg",
+       title:" Hàng trăm người mang logo 'uống rượu không lái xe' tiễn đưa cô giáo",
+   },
+   
+];
+module.exports = mang;
